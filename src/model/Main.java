45cbc9358c26038cdee07a575b17538fcad5abd8
Mike Witten <micheal.l.witten@gmail.com>
2022-03-09 15:35:28 -0600

    Initial commit

---
+++ src/model/Main.java
@@ -0,0 +1,22 @@
+package model;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start (Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("file"));
+        stage.setTitle("Title");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
